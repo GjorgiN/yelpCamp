1a1ded658140776554dc7e59afa154c6bf26fa17
GjorgiN <gjorgi.nikolovski@gmail.com>
2022-09-21 18:23:36 +0200

    Starting up

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+const ejs = require('ejs');
+const path = require('path');
+
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.get('/', (req, res) => {
+    // res.send('HELLO FROM YELP CAMP');
+    res.render('home')
+});
+
+
+app.listen(3000, () => {
+    console.log('LISTENING ON PORT 3000');
+})
