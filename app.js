6a7ab4671a8168352b27472d2b470f761f1474c7
GjorgiN <gjorgi.nikolovski@gmail.com>
2022-09-22 23:55:57 +0200

    s39 CRUD basics

--- app.js
+++ app.js
@@ -1,18 +1,66 @@
 const express = require('express');
-const app = express();
-
+const mongoose = require('mongoose');
 const ejs = require('ejs');
 const path = require('path');
+const Campground = require('./models/campground');
+const methodOverride = require('method-override');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp')
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => console.log('Database connected'));
+
+const app = express();
 
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(methodOverride('_method'));
+
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
     // res.send('HELLO FROM YELP CAMP');
     res.render('home')
 });
 
+app.get('/campgrounds', async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', { campgrounds });
+})
+
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+})
+
+app.post('/campgrounds/new', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground.id}`);
+})
+
+app.get('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show', { campground });
+})
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findByIdAndUpdate(req.params.id, { ...req.body.campground });
+    await campground.save()
+    res.redirect(`/campgrounds/${campground._id}`);
+})
+
+app.delete('/campgrounds/:id', async (req, res) => {
+    await Campground.findByIdAndDelete(req.params.id);
+    res.redirect(`/campgrounds`);
+})
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', { campground });
+})
 
 app.listen(3000, () => {
     console.log('LISTENING ON PORT 3000');
