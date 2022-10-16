d488c6368268f4266df59d538ffb2c63150a9afc
GjorgiN <gjorgi.nikolovski@gmail.com>
2022-10-17 00:02:39 +0200

    move all middlewares in separate file

--- middleware.js
+++ middleware.js
@@ -1,8 +1,52 @@
+const ExpressError = require('./utils/ExpressError');
+const { campgroundSchema, reviewSchema } = require('./schemas');
+const Campground = require('./models/campground');
+const Review = require('./models/review');
+
+
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
         req.session.returnTo = req.originalUrl;
         req.flash('error', 'You need to be logged in first.');
         return res.redirect('/login');
     }
+    next();
+}
+
+module.exports.validateCampground = (req, res, next) => {
+    const { error } = campgroundSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(detail => detail.message).join(',');
+        throw new ExpressError(msg, 400);
+    }
+    next();
+}
+
+module.exports.isAuthor = async (req, res, next) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    if (!campground.author._id.equals(req.user._id)) {
+        req.flash('error', 'You cannot edit this campground. You are not the author...');
+        return res.redirect(`/campgrounds/${id}`);
+    }
+    next();
+}
+
+module.exports.validateReview = (req, res, next) => {
+    const { error } = reviewSchema.validate(req.body);
+    if (error) {
+        const msg = error.details.map(detail => detail.message).join(',');
+        throw new ExpressError(msg, 400);
+    }
+    next();
+}
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+    const { id, reviewId } = req.params;
+    const review = await Review.findById(reviewId);
+    if (!review.author._id.equals(req.user._id)) {
+        req.flash('error', 'Cannot delete others users reviews!');
+        return res.redirect(`/campgrounds/${id}`);
+    };
     next();
 }
