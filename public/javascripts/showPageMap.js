d92c1430d0413bf443dffc4a50eb6ecfb4edd0a8
GjorgiN <gjorgi.nikolovski@gmail.com>
2022-10-23 19:04:51 +0200

    mapbox map, point and popup

---
+++ public/javascripts/showPageMap.js
@@ -0,0 +1,20 @@
+mapboxgl.accessToken = mapToken;
+const campgroundParsed = JSON.parse(campgroundStringified);
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/light-v9', // style URL
+    center: campgroundParsed.geometry.coordinates, // starting position [lng, lat]
+    zoom: 7, // starting zoom
+    projection: 'globe' // display the map as a 3D globe
+});
+
+map.on('style.load', () => {
+    map.setFog({}); // Set the default atmosphere style
+});
+
+const marker1 = new mapboxgl.Marker()
+    .setLngLat(campgroundParsed.geometry.coordinates)
+    .setPopup(new mapboxgl.Popup({ closeOnClick: true })
+        .setHTML(`<h6>${campgroundParsed.title}</h6><p>${campgroundParsed.location}</p>`))
+    .addTo(map);
+
