647d40d39ee9336a869eab6e7e87d45cb5a6c039
GjorgiN <gjorgi.nikolovski@gmail.com>
2022-11-08 03:24:01 +0100

    recentering cluster map

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -4,8 +4,8 @@ const map = new mapboxgl.Map({
     container: 'cluster-map',
     // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
     style: 'mapbox://styles/mapbox/light-v10',
-    center: [-103.5917, 40.6699],
-    zoom: 3
+    center: [0.00, 31.00],
+    zoom: 2
 });
 
 map.on('load', () => {
